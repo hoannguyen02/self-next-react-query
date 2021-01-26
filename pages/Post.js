4669cb0490fdd82180e351eb091783292d51edb0
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2021-01-26 20:02:35 +0700

    Add basic navigation

---
+++ pages/Post.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Post() {
+  return (
+    <h2>
+      <Link href='/'>Back</Link>
+    </h2>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 const Home = () => (
   <div>
     <h2 className='text-center'>Welcome to self nextjs & react-query</h2>
+    <Link href='/post'>Post Page</Link>
+
     <style jsx>{`
       .text-center {
         text-align: center;
